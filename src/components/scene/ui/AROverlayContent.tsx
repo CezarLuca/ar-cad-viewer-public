d515c53dc1b7b7ad64f8ae445781a4faacabf6f3
Cezar Luca <cezar.luca@hotmail.com>
2025-04-03 17:23:55 +0200

    Remove the QR code tracker to be replaced with AR.js

--- src/components/scene/ui/AROverlayContent.tsx
+++ src/components/scene/ui/AROverlayContent.tsx
@@ -1,18 +1,11 @@
-import { Vector3 } from "three";
 import ModelControls from "./ModelControls";
 
 export default function AROverlayContent({
     onPlaceModel,
     isModelPlaced,
-    currentHitPosition,
-    qrDetected,
-    qrContent,
 }: {
     onPlaceModel: () => void;
     isModelPlaced: boolean;
-    currentHitPosition: Vector3 | null;
-    qrDetected: boolean;
-    qrContent: string;
 }) {
     return (
         <div className="absolute top-12 left-0 right-0 bottom-0 z-20 pointer-events-none">
@@ -25,26 +18,6 @@ export default function AROverlayContent({
                         >
                             Place Model
                         </button>
-                    </div>
-                    {/* Debug info */}
-                    <div className="absolute top-5 left-14 bg-black bg-opacity-50 text-white p-2 rounded pointer-events-none">
-                        {currentHitPosition
-                            ? `Hit: (${currentHitPosition.x.toFixed(
-                                  2
-                              )}, ${currentHitPosition.y.toFixed(
-                                  2
-                              )}, ${currentHitPosition.z.toFixed(2)})`
-                            : "No hit detected"}
-                        <br />
-                        Status:{" "}
-                        {isModelPlaced
-                            ? "Model Placed"
-                            : "Waiting for placement"}
-                        <br />
-                        QR:{" "}
-                        {qrDetected
-                            ? `Detected (${qrContent.substring(0, 15)}...)`
-                            : "None"}
                     </div>
                 </>
             )}
