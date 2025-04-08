11205226b569b90a64eef35f8099ec3316f36237
Cezar Luca <cezar.luca@hotmail.com>
2025-04-08 21:04:47 +0200

    Simplify ARContext

--- src/context/ARContext.tsx
+++ src/context/ARContext.tsx
@@ -1,20 +1,6 @@
 "use client";
 
-import React, {
-    createContext,
-    useContext,
-    useState,
-    useRef,
-    useEffect,
-} from "react";
-
-interface XRSessionInitExtended extends XRSessionInit {
-    trackedImages?: {
-        // image: Promise<ImageBitmap>;
-        image: ImageBitmap;
-        widthInMeters: number;
-    }[];
-}
+import React, { createContext, useContext, useState, useRef } from "react";
 
 interface ARContextValue {
     isARPresenting: boolean;
@@ -35,78 +21,13 @@ export const ARProvider: React.FC<{ children: React.ReactNode }> = ({
 }) => {
     const [isARPresenting, setIsARPresenting] = useState(false);
     const containerRef = useRef<HTMLDivElement>(null);
-    const [imageBitmapPromise, setImageBitmapPromise] =
-        useState<Promise<ImageBitmap> | null>(null);
-
-    // Load the image and create an ImageBitmap on the client side
-    useEffect(() => {
-        const loadTrackedImage = async () => {
-            const image = new Image();
-            image.src = "/markers/qrTracker.png";
-            await image.decode();
-            const bitmap = createImageBitmap(image);
-            setImageBitmapPromise(bitmap);
-        };
-
-        loadTrackedImage().catch((error) => {
-            console.error("Error loading tracked image:", error);
-        });
-    }, []);
 
     const enterAR = async () => {
-        if (navigator.xr && containerRef.current && imageBitmapPromise) {
-            const isSupported = await navigator.xr.isSessionSupported(
-                "immersive-ar"
-            );
-            if (isSupported) {
-                try {
-                    const session = await navigator.xr.requestSession(
-                        "immersive-ar",
-                        {
-                            requiredFeatures: [
-                                "local-floor",
-                                "dom-overlay",
-                                "image-tracking",
-                            ],
-                            trackedImages: [
-                                {
-                                    image: await imageBitmapPromise, // Wait for the promise to resolve
-                                    widthInMeters: 0.1, // Specify the real-world size
-                                },
-                            ],
-                            domOverlay: { root: containerRef.current },
-                        } as XRSessionInitExtended
-                    );
-
-                    session.addEventListener("end", () => {
-                        console.log("AR session ended.");
-                        setIsARPresenting(false);
-                    });
-
-                    session.updateRenderState({
-                        baseLayer: new XRWebGLLayer(
-                            session,
-                            document
-                                .createElement("canvas")
-                                .getContext("webgl")!
-                        ),
-                    });
-
-                    setIsARPresenting(true);
-                    console.log("AR session started!");
-                } catch (error) {
-                    console.error("Failed to start AR session:", error);
-                }
-            } else {
-                console.error("AR not supported on this device.");
-            }
-        }
+        setIsARPresenting(true);
     };
 
     const exitAR = () => {
-        // In a real app, you would call session.end() if you have stored the session.
         setIsARPresenting(false);
-        console.log("AR session exited.");
     };
 
     return (
